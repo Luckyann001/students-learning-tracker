3b1ce2336110bebdf7fa99a7d1d33631012f56a2
Luckyann Kagendo <kagendoluckyann@gmail.com>
2025-10-28 11:24:23 +0300

    changed subjectlist.jsx

--- src/components/SubjectList.jsx
+++ src/components/SubjectList.jsx
@@ -1,11 +1,14 @@
+// src/components/SubjectList.jsx
 import React from "react";
-import SubjectCard from "./SubjectCard";
 
 export default function SubjectList({ subjects }) {
   return (
     <div className="subject-list">
-      {subjects.map((sub) => (
-        <SubjectCard key={sub.id} subject={sub} />
+      {subjects.map((subject) => (
+        <div key={subject.id} className="subject-item">
+          <h4>{subject.name}</h4>
+          <p>{subject.progress}%</p>
+        </div>
       ))}
     </div>
   );
